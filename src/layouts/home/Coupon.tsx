b284af37cf5cd95882328b9008e12e48de70120e
PhamTrungNguyen <trungnguyen010102hl@gmail.com>
2023-02-16 00:10:00 +0700

    fe

--- src/layouts/home/Coupon.tsx
+++ src/layouts/home/Coupon.tsx
@@ -8,6 +8,8 @@ const settings: {} = {
   speed: 700,
   slidesToShow: 4,
   slidesToScroll: 2,
+  nextArrow: <SampleNextArrow />,
+  prevArrow: <SamplePrevArrow />,
   responsive: [
     {
       breakpoint: 1024,
@@ -27,10 +29,10 @@ const settings: {} = {
 };
 const Coupon = () => {
   return (
-    <div className="pl-[40px] pr-[20px] max-lg:pl-[20px] pb-[40px] max-md:pb-[20px] max-lg:pr-[10px]">
+    <div className="pl-[20px] pr-[40px] max-lg:pr-[20px] max-lg:pl-[10px]">
       <Slider
         {...settings}
-        className="cursor-pointer flex flex-row justify-between  "
+        className="cursor-pointer flex flex-row justify-between pb-[40px] max-md:pb-[20px] "
       >
         <CouponItem icon={coupon_10}></CouponItem>
         <CouponItem icon={coupon_10}></CouponItem>
@@ -40,5 +42,13 @@ const Coupon = () => {
     </div>
   );
 };
+function SampleNextArrow(props: any) {
+  const { className, onClick } = props;
+  return <div className={`${className} next_coupon`} onClick={onClick} />;
+}
 
+function SamplePrevArrow(props: any) {
+  const { className, style, onClick } = props;
+  return <div className={`${className} prev_coupon`} onClick={onClick} />;
+}
 export default Coupon;
